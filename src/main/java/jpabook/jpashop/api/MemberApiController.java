e9f66448c1911043bc2b3e47a0eee6add2a3e792
Alal11 <spalqj56@naver.com>
2024-09-04 14:56:40 +0900

    [API 개발 기본] 회원 수정 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -4,11 +4,10 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -48,4 +47,30 @@ public class MemberApiController {
             this.id = id;
         }
     }
+
+
+    /**
+     * 수정 API
+     */
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberResponse(@PathVariable("id") Long id,
+                                                     @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
 }
